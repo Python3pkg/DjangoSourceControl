13f5accfeadbfdfb2bacb0f35a920e352233bff3
dpepka <dpepka@eecs.wsu.edu>
2016-12-05 21:43:52 -0800

    Adding virtualenv
    Both dsc and server have their own virtualenv to seperate conserns.

---
+++ server/serverenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\cpts481_homeowrk\DjangoTest\Cpts481FinalProject\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
